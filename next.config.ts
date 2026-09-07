67a1d50ad426ccf66d9ab803330726a9a8489ba5
Filmsori <nnorbert2586@gmail.com>
2026-09-07 14:30:11 +0200

    gf+

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/film/:path*',
+        destination: '/filmek/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
